cf81ec3beb9f4a7731fb8df9e7ca2c74854bd925
EHerbepin <118301954+EHerbepin@users.noreply.github.com>
2022-12-02 15:43:28 +0100

    Update Script_principal-HDV-EPIC.R

--- Script_principal-HDV-EPIC.R
+++ Script_principal-HDV-EPIC.R
@@ -1,196 +1,144 @@
+
+##1. Population religieuse 
+
+#Ici nous allons briévement représenter la population religieuse en France en 2003 (HDV) et en 2013 (EPIC) avec 2 questions. 
+
+#1- Nous allons pouvoir faire un comparatif de 10 ans vis à vis de la population religieuse. 
+#2- Nous ferons également un comparatif entre homme et femmes, religieux.ses entre 2003 et 2013, puis dans son ensemble.
+
 library(foreign)
 library(questionr)
 library(survey)
 library(R2HTML)
 library(tidyverse)
 library(esquisse)
+library(labelled)
+library(DataExplorer)
+library(funModeling)
+library(missForest)
+library(dplyr)
+library(tinytex)
 
-##La base de données HDV étant en 4 parties, 
+#Exploration et nettoyage de la base HDV2003: 
+
+##La base de donnees HDV était en 4 parties, 
 ##nous avons ouvert la premiere et la quatrieme...
+
 hdv1<-read.dbf("hdv1.dbf")
 hdv4<-read.dbf("hdv4.dbf")
 
-##...pour récupérer les variables qui nous interressent.
+##...pour récuperer les variables qui nous interressent.
+
 hdv <- cbind(select(hdv1, c(AGEE,AGECJ)), select(hdv4, c(STATUTE,STATUTCJ,NIVE2TE,NIVE2TCJ,QUALIFE,QUALCJ,SEXEE,SEXECJ,VRELIG,VENREL,VIECOUE)))
 
 ## Puis nous nettoyons notre environnement
+
 rm(hdv1,hdv4)
 
-## Pour EPIC, nous avons ouvert la base de données EPIC pour y recuperer ce qu'il nous faut :
-epic <- select(read.csv("repondant.txt", sep="\t"),c(IndCOU,R_IMPREL,R_RELIGC,R_RELIGION,C_DIPLOMEC,M_DIPLOME,C_CS13C,M_CS13,SEXER,H_SEXEC_C,AGEM,H_ANAISC_C,R_MOINDIPL,R_PLUDIPL))
+#appeler la script Recodage contenant les traitements des variables de la base hdv:
 
-##1. Population religieuse 
+source("recodage_hdv2003.R")
 
-#Ici nous allons brièvement représenté la population religieuse en France en 2003 (HDV) et en 2013 (EPIC) avec 2 questions. 
+#representer les données: 
 
-#1- Nous allons pouvoir faire un comparatif de 10 ans vis à vis de la population religieuse. 
-#2- Nous ferons également un comparatif entre homme et femmes, religieux.ses entre 2003 et 2013, puis dans son ensemble.
+head(hdv)
 
-##Par soucis de lisibilité, nous allons recoder les variables et leurs modalités.
+#les dimensions de la base hdv:
 
-#Exemple à faire pour TOUT - Faire un trie à plat pour chaque variable.
+dim(hdv)
+describe(hdv)
+str(hdv)
 
-#Trie à plat pour la variable VRELIG 
-hdv$VRELIG <- as.factor(hdv$VRELIG) #je transforme ma variable en facteur.
+#appeler le script exploration de données :
 
-hdv$VRELIG <- fct_recode(hdv$VRELIG,
-                              "Pratique régulière"="1",
-                              "Pratique occasionnelle"="2",
-                              "Sentiment d'appartenance sans pratique"="3",
-                              "Ni pratique ni appartenance"="4",
-                              "Rejet"="5",
-                              "NSP"="6")
+source("exploration_hdv2003.R")
 
-table(hdv$VRELIG)
+## Pour la base EPIC, nous avons ouvert la base de donnees EPIC pour y recuperer ce qu'il nous faut :
 
+epic <- select(read.csv("repondant.txt", sep="\t"),
+               c(IndCOU,R_IMPREL,R_RELIGC,R_RELIGION,C_DIPLOMEC,M_DIPLOME,C_STATUTC,M_STATUT,SEXER,H_SEXEC_C,AGEM,H_ANAISC_C,R_MOINDIPL,R_PLUDIPL))
 
-#Trie à plat pour la variable VENREL
+#appeler la script Recodage contenant les traitements des variables de la base epic:
 
-hdv$VENREL <- as.factor(hdv$VENREL) #je transforme ma variable en facteur.
+source("recodage_epic.R")
 
-hdv$VENREL <- fct_recode(hdv$VENREL,
-                         "Important que ses enfants partagent la même croyance religieuse"="1",
-                         "Pas d'importance si les enfants ne partagent pas la même croyance religieuse"="2",)
+#Representer les données :
 
-table(hdv$VENREL)
+head(epic)
 
-#Trie à plat pour la variable NIV2TE - Niveau d'étude de l'enquêté
+#Les dimensions de la base: 
 
-hdv$NIVE2TE <- as.factor(hdv$NIVE2TE) #je transforme ma variable en facteur.
+dim(epic)
+str(epic)
 
-hdv$NIVE2TE <- fct_recode(hdv$NIVE2TE,
-                         "N'a jamais fait d'études"="00",
-                         "À arreté avant la dernière année de primaire"="03",
-                         "Arret la dernière année de primaire"="04",
-                         "1er cycle d'enseignement général"="15",
-                         "2ème sycle d'enseignement général"="17",
-                         "Enseignement technique ou professionnel court"="20", 
-                         "Enseignement technique ou professionnel long"="30", 
-                         "Enseignement supérieur y compris technique supérieur"="40")
+#appeler le script exploration de données :
 
-table(hdv$NIVE2TE)
+source("exploration_epic.R")
 
-#Trie à plat pour la variable NIVE2TCJ - NIveau d'étude du conjoint
+#Comparaison :
 
-hdv$NIVE2TCJ <- as.factor(hdv$NIVE2TCJ) #je transforme ma variable en facteur.
+#Nous allons pouvoir faire un comparatif de 10 ans vis à vis de la population religieuse en fonction de l'age. 
 
-hdv$NIVE2TCJ <- fct_recode(hdv$NIVE2TCJ,
-                          "N'a jamais fait d'études"="00",
-                          "À arreté avant la dernière année de primaire"="03",
-                          "Arret la dernière année de primaire"="04",
-                          "1er cycle d'enseignement général"="15",
-                          "2ème sycle d'enseignement général"="17",
-                          "Enseignement technique ou professionnel court"="20", 
-                          "Enseignement technique ou professionnel long"="30", 
-                          "Enseignement supérieur y compris technique supérieur"="40", 
-                          "Ne sait pas"="99")
+library(ggplot2)
+ggplot(hdv) +
+ aes(x = "", y = Age_enquete, fill = Rapport_religion) +
+ geom_boxplot() +
+ scale_fill_hue(direction = 1) +
+ theme_minimal()
 
-table(hdv$NIVE2TCJ)
+#on constate que les pratiquants régulieres ont l'age autour de 60 ans, alors que les gens ni pratiquants ni appartants ont l'age autour de 40 ans
 
+library(ggplot2)
+ggplot(epic) +
+ aes(x = "", y = Age_individu, fill = Importance_religion_enquete) +
+ geom_boxplot() +
+ scale_fill_hue(direction = 1) +
+ theme_minimal()
 
-#Trie à plat pour la variable STATUTE - Statut ou dernier statut professionnel de l'enquêté
+#les ages pour les 4 premieres modalités est presque le meme, il est autour de 48 ans! 
 
-hdv$STATUTE <- as.factor(hdv$STATUTE) #je transforme ma variable en facteur.
+#Nous ferons  un comparatif entre homme et femmes, religieux.ses entre 2003 et 2013:
 
-hdv$STATUTE <- fct_recode(hdv$STATUTE,
-                          "Salarié de l'Etat"="1",
-                          "Salarié d'une collectivité locale"="2",
-                          "Salarié d'une entreprise publique ou nationale"="3",
-                          "Salarié du secteur privé"="4",
-                          "Salarié de son entreprise ou salarié de son conjoint"="5",
-                          "Non salarié : aide ou aidait un membre de sa famille dans son travail sans être salarié"="6", 
-                          "Non salarié : A son compte, indépendant, employeur, gérant, associé d'une société, co-exploitant"="7")
+library(ggplot2)
+ggplot(hdv) +
+ aes(x = Rapport_religion, fill = Sexe_enquete) +
+ geom_bar() +
+ scale_fill_hue(direction = 1) +
+ theme_minimal()
 
-table(hdv$STATUTE)
+#pour les pratiquants régulieres et ocasionneles , les hommes sont plus frequents que les femmes, alors que dans les autres modalités 
+#ils sont egaux!
 
-#Trie à plat pour la variable STATUTCJ - Statut ou dernier statut professionnel du conjoint.
+library(ggplot2)
+ggplot(epic) +
+ aes(x = Importance_religion_enquete, fill = Sexe_enquete) +
+ geom_bar() +
+ scale_fill_hue(direction = 1) +
+ theme_minimal()
 
-hdv$STATUTCJ <- as.factor(hdv$STATUTCJ) #je transforme ma variable en facteur.
-
-hdv$STATUTCJ <- fct_recode(hdv$STATUTCJ,
-                           "Salarié de l'Etat"="1",
-                           "Salarié d'une collectivité locale"="2",
-                           "Salarié d'une entreprise publique ou nationale"="3",
-                           "Salarié du secteur privé"="4",
-                           "Salarié de son entreprise ou salarié de son conjoint"="5",
-                           "Non salarié : aide ou aidait un membre de sa famille dans son travail sans être salarié"="6", 
-                           "Non salarié : A son compte, indépendant, employeur, gérant, associé d'une société, co-exploitant"="7",
-                           "Ne sait pas"="9")
-
-table(hdv$STATUTCJ)
-
-#Trie à plat de la variable QUALIFE - Position professionnel de l'enquêté 
-
-hdv$QUALIFE <- as.factor(hdv$QUALIFE) #je transforme ma variable en facteur.
-
-hdv$QUALIFE <- fct_recode(hdv$QUALIFE,
-                          "Manoeuvre ou ouvrier spécialisé"="1",
-                          "Ouvrier qualifié ou technicien.ne d'atelier"="2",
-                          "Technicien.ne non cadre"="3",
-                          "Agent administratif ou commerciaux"="4",
-                          "Ingénieur ou cadre"="5",
-                          "Employé ou personnel"="6", 
-                          "Autre"="7")
-
-table(hdv$QUALIFE)
-
-#Trie à plat de la variable QUALCJ - position professionnel du conjoint
-
-hdv$QUALCJ <- as.factor(hdv$QUALCJ) #je transforme ma variable en facteur.
-
-hdv$QUALCJ <- fct_recode(hdv$QUALCJ,
-                          "Manoeuvre ou ouvrier spécialisé"="1",
-                          "Ouvrier qualifié ou technicien.ne d'atelier"="2",
-                          "Technicien.ne non cadre"="3",
-                          "Agent administratif ou commerciaux"="4",
-                          "Ingénieur ou cadre"="5",
-                          "Employé ou personnel"="6", 
-                          "Autre"="7",
-                          "NSP"="9")
-
-table(hdv$QUALCJ)
-
-###Il faudrait ici combiner les QUAL avec les STATUT pour former de vraies catérogies professionnelles.
+#pour les gens qui ont déclaré que la réligion est important, assez important, et peu important les hommes sont les plus representés, alors que pour les autres modalités, ils sont égaux! 
 
 
-#Trie à plat des variables de bases : SEXEE (sexe enquêté), SEXECJ (sexe conjoint), AGEE (age enquêté), AGECJ (age conjoint).
+#En suite on souhaite savoir si les individus qui ont declaré que la réligion est trop important peuvent accepter d'etre avec quelqu'un qui est moins diplomé ou plus diplomé : 
 
-#Sexe enquêté 
-hdv$SEXEE <- as.factor(hdv$SEXEE) #je transforme ma variable en facteur.
+ggplot(epic) +
+  aes(x = Importance_religion_enquete, fill = Accepter_etre_avec_moins_diplome) +
+  geom_bar(position = "dodge") +
+  scale_fill_manual(values = c(`oui, c'est justement mon cas` = "#0FDB3F", oui = "#246D1E", non = "#AC2927", 
+                               `Ne sait pas` = "#252506")) +
+  theme_minimal()
 
-hdv$SEXEE <- fct_recode(hdv$SEXEE,
-                         "Homme"="1",
-                         "Femme"="2",)
+#Selon ce graphique, on vois bien que les gens répoondant oui à cette question sont la categorie la plus representé parmi les autres
+#En plus, ceux appartenants à la modalité (peu important) sont les plus à déclarer oui (accepter d'etre avec qqn moins diplomé)!
+#parmi ceux qui ont répondu non, sont le plus nombreux dans la modalité (important ou assez important)
 
-table(hdv$SEXEE)
+ggplot(epic) +
+  aes(x = Importance_religion_enquete, fill = Accepter_etre_avec_plus_diplome) +
+  geom_bar(position = "dodge") +
+  scale_fill_manual(values = c(`oui, c'est justement mon cas` = "#0FDB3F", oui = "#246D1E", non = "#AC2927", 
+                               `Ne sait pas` = "#252506")) +
+  theme_minimal()
 
-#Sexe conjoint 
-hdv$SEXECJ <- as.factor(hdv$SEXECJ) #je transforme ma variable en facteur.
-
-hdv$SEXECJ <- fct_recode(hdv$SEXECJ,
-                        "Homme"="1",
-                        "Femme"="2",)
-
-table(hdv$SEXECJ)
-
-
-#age enquêté 
-
-table(hdv$AGEE)
-
-#age conjoint
-
-table(hdv$AGECJ)
-
-
-
-
-
-
-
-addmargins(table(hdv$VRELIG)) #Donne la somme
-
-freq(hdv$VRELIG)# Valeurs en %
-
-
-table(hdv$VRELIG,hdv$SEXEE)#Croiser 2 var
+#Selon ce graphique, on vois bien que les gens répoondant oui à cette question sont la categorie la plus representé parmi les autres
+#En plus, ceux appartenants à la modalité (peu important) sont les plus à déclarer oui (accepter d'etre avec qqn plus diplomé)!
+#parmi ceux qui ont répondu non, sont le plus nombreux dans la modalité (important ou assez important)
